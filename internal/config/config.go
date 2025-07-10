7257a646da4bcf1af7a8584950bec91fa2effed0
Andrey Matveyev <a.v.matveyev@gmail.com>
2025-07-10 17:09:29 +0300

    Added logger to cinfig

--- internal/config/config.go
+++ internal/config/config.go
@@ -10,24 +10,28 @@ import (
 
 type Config struct {
 	Environment    string `yaml:"environment" env-default:"local"` // local, dev, prod
-	RepositoryPath string `yaml:"repository_path" env-required:"true"`
-	HTTPServer     `yaml:"http_server"`
+	RepositoryFile string `yaml:"repository-file" env-required:"true"`
+	HTTPServer     `yaml:"http-server"`
+	Logger         `yaml:"logger"`
 }
 
 type HTTPServer struct {
 	Address     string        `yaml:"address" env-default:"localhost:8080"`
 	Timeout     time.Duration `yaml:"timeout" env-default:"5s"`
-	IdleTimeout time.Duration `yaml:"idle_timeout" env-default:"60s"`
+	IdleTimeout time.Duration `yaml:"idle-timeout" env-default:"60s"`
+}
+
+type Logger struct {
+	Format          string `yaml:"format" nv-default:"text"`         // text, json
+	Destination     string `yaml:"destination" nv-default:"console"` // console, file
+	DestinationFile string `yaml:"destination-file" nv-default:""`   // filename if destination = file
+	Level           string `yaml:"level" nv-default:"debug"`         // debug, info
+	AddSource       bool   `yaml:"add-source" nv-default:"false"`
 }
 
 // The function terminates the application with an error if the configuration file could not be loaded.
 // In this case, we use the default logger (our main logger is not yet initialized at this stage)
 func ReadConfig(path string) *Config {
-	/*configPath, ok := os.LookupEnv(path)
-	if !ok {
-		log.Fatal("CONFIG_PATH is not set")
-	}*/
-
 	// check if file exists
 	if _, err := os.Stat(path); os.IsNotExist(err) {
 		log.Fatalf("config file does not exist: %s", path)
