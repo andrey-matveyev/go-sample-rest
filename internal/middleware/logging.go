989dc6ed217b79250010e71a6f73e8f8732cebd9
Andrey Matveyev <a.v.matveyev@gmail.com>
2025-07-12 00:01:09 +0300

    CleanUp

--- internal/middleware/logging.go
+++ internal/middleware/logging.go
@@ -11,7 +11,6 @@ func LoggingMiddleware(next http.Handler) http.Handler {
 		ctxLogger := slog.Default().With(
 			slog.String("RequestURI", r.RequestURI),
 			slog.String("Method", r.Method),
-			slog.String("URL_Path", r.URL.Path),
 		)
 		ctx := logger.ContextWithLogger(r.Context(), ctxLogger)
 		next.ServeHTTP(w, r.WithContext(ctx))
