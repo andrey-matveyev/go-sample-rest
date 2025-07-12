30aba90fdf21797f75b779f6e2a392864719ff0f
Andrey Matveyev <a.v.matveyev@gmail.com>
2025-07-12 16:38:04 +0300

    Added RequestID to logs

--- internal/middleware/logging.go
+++ internal/middleware/logging.go
@@ -4,15 +4,18 @@ import (
 	"log/slog"
 	"main/internal/logger"
 	"net/http"
+
+	chi_mw "github.com/go-chi/chi/v5/middleware"
 )
 
 func LoggingMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		ctxLogger := slog.Default().With(
+			slog.String("RequestID", chi_mw.GetReqID(r.Context())), // depends on chi-middleware
 			slog.String("RequestURI", r.RequestURI),
 			slog.String("Method", r.Method),
 		)
 		ctx := logger.ContextWithLogger(r.Context(), ctxLogger)
-		next.ServeHTTP(w, r.WithContext(ctx))
+		next.ServeHTTP(w, r.WithContext(ctx)) // logger in context
 	})
 }
